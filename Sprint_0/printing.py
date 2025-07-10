2c42aa6ff098404647296ac4d24d767360be294f
alpha01 <salekbenelhabchi@gmail.com>
2025-07-10 16:15:39 +0100

    first printing statements

---
+++ Sprint_0/printing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a simple script that prints various data types"""
+name: str = "Ahmed"
+Age: int = 43
+Balance: float = 315.00
+is_subscribed: bool = False
+
+print(f"Hi {name:s},")
+print(f"Your current balance is : ${Balance:>5.2f}")
+print(f"Subsribtion : {is_subscribed:>13b}")
---
+++ Sprint_0/zen_of_python.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""Printing the Zen of python"""
+import this
