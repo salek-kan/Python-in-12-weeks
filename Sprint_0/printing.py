9b68ad4f7cc19554afdec8fb71d165715ed237a1
alpha01 <salekbenelhabchi@gmail.com>
2025-07-11 03:23:53 +0100

    fixed var. names

--- Sprint_0/printing.py
+++ Sprint_0/printing.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This is a simple script that prints various data types"""
 name: str = "Ahmed"
-Age: int = 43
-Balance: float = 315.00
+age: int = 43
+balance: float = 315.00
 is_subscribed: bool = False
 
 print(f"Hi {name:s},")
-print(f"Your current balance is : ${Balance:>5.2f}")
+print(f"Your current balance is : ${balance:>5.2f}")
 print(f"Subscribtion : {is_subscribed:>12b}")
